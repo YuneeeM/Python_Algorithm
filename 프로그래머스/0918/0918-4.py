82989eedd5af5f3d5e7fce5629e5c7d39177d7c8
YuneeeM <dana512@naver.com>
2023-09-19 02:00:48 +0900

    0918 완료

--- 프로그래머스/0918/0918-4.py
+++ 프로그래머스/0918/0918-4.py
@@ -1,1 +1,12 @@
 # 배열 뒤집기
+
+def solution(num_list):
+    answer = []
+    n = len(num_list)
+    n = n+1
+    print(n)
+    i = -1
+    for i in range(-1, -n, -1):
+        cur = num_list[i]
+        answer.append(cur)
+    return answer
