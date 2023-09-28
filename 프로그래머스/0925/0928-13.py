403c3cd69b414919e62834768809e1f4889c378b
YuneeeM <dana512@naver.com>
2023-09-28 20:05:49 +0900

    0928 완료

--- 프로그래머스/0925/0928-13.py
+++ 프로그래머스/0925/0928-13.py
@@ -1,1 +1,26 @@
 # 영어가 싫어요
+def solution(numbers):
+    answer = ''
+    arr = ["zero", "one", "two", "three", "four",
+           "five", "six", "seven", "eight", "nine"]
+    if x in arr:
+        n = arr.index(x)
+        answer += n
+
+    return answer
+
+
+if __name__ == "__main__":
+    numbers = "onetwothreefourfivesixseveneightnine"
+    # print(solution)
+
+    answer = ''
+
+    arr = ["zero", "one", "two", "three", "four",
+           "five", "six", "seven", "eight", "nine"]
+
+    if numbers in arr:
+        n = arr.index(numbers)
+        answer += n
+
+    print(answer)
--- 프로그래머스/0925/0928-14.py
+++ 프로그래머스/0925/0928-14.py
@@ -1,1 +1,23 @@
 # 인덱스 바꾸기
+def solution(numbers):
+    arr = ["zero", "one", "two", "three", "four",
+           "five", "six", "seven", "eight", "nine"]
+    for k, v in enumerate(arr):
+        numbers = numbers.replace(v, str(k))
+
+    return int(numbers)
+
+
+'''
+def solution(numbers):
+    answer = 0
+    arr=["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    
+    for x in arr:
+        if x in numbers:
+            answer=answer*10+arr.index(x)
+        
+    return answer
+
+0을 인식 못해서 위 방법으로 해결함
+'''
