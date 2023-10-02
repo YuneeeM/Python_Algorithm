e602172178cc78a5aee16a3014b895ff81024467
YuneeeM <dana512@naver.com>
2023-10-03 02:46:31 +0900

    1003 -1

---
+++ 프로그래머스/1002/1003-6.py
@@ -0,0 +1,5 @@
+# 캐릭터의 좌표
+
+board = [12, 11]
+print(board[0])
+print(board[1])
---
+++ 프로그래머스/1002/1003-7.py
@@ -0,0 +1,4 @@
+# 최댓값 만들기 (2)
+def solution(numbers):
+    numbers.sort()
+    return max(numbers[0]*numbers[1], numbers[-1]*numbers[-2])
