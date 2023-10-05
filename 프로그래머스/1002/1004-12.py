264270442f1b79461a4fe993e98cbadeadd8c092
YuneeeM <dana512@naver.com>
2023-10-06 00:26:40 +0900

    1004 완료

--- 프로그래머스/1002/1004-12.py
+++ 프로그래머스/1002/1004-12.py
@@ -1,1 +1,17 @@
 # 외계어 사전
+def solution(spell, dic):
+    answer = 0
+    n = len(spell)
+    arr = []
+    for x in dic:
+        answer = 0
+        for i in spell:
+            if i in x:
+                answer += 1
+        arr.append(int(answer))
+
+    for x in arr:
+        if x == n:
+            return 1
+    else:
+        return 2
--- 프로그래머스/1002/1004-9.py
+++ 프로그래머스/1002/1004-9.py
@@ -1,1 +1,15 @@
 # 숨어있는 숫자의 덧셈 (2)
+def solution(my_string):
+
+    answer = 0
+
+    for x in my_string:
+        if x.isalpha():
+            my_string = my_string.replace(x, ' ')
+
+    my_string = my_string.split()
+
+    for x in my_string:
+        answer += int(x)
+
+    return answer
