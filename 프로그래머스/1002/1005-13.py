b1b8b7ae7177798318ae3a153d1b4fe6d6245bf0
YuneeeM <dana512@naver.com>
2023-10-07 01:34:07 +0900

    1005 - 2

--- 프로그래머스/1002/1005-13.py
+++ 프로그래머스/1002/1005-13.py
@@ -1,1 +1,8 @@
 # 저주의 숫자 3
+def solution(n):
+    answer = 0
+    for x in range(1, n+1):
+        answer += 1
+        while answer % 3 == 0 or '3' in str(answer):
+            answer += 1
+    return answer
