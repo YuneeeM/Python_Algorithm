9ee572edda7fc6cafa55b9c390975652f24e2d1e
YuneeeM <dana512@naver.com>
2023-10-08 23:28:55 +0900

    1008 완료

--- 프로그래머스/1002/1008-17.py
+++ 프로그래머스/1002/1008-17.py
@@ -1,1 +1,19 @@
 # 특이한 정렬
+
+def custom_sort(item):
+    return (item[0], -item[1])
+
+
+def solution(numlist, n):
+    answer = []
+    arr = []
+    num = 0
+    numlist.sort(reverse=True)
+    for x in numlist:
+        num = abs(x-n)
+        answer.append([num, x])
+
+    answer = sorted(answer, key=custom_sort)
+    for a, b in answer:
+        arr.append(b)
+    return arr
--- 프로그래머스/1002/1008-18.py
+++ 프로그래머스/1002/1008-18.py
@@ -1,1 +1,15 @@
 # 등수 매기기
+
+def solution(score):
+    answer = []
+    res = []
+
+    for x in score:
+        res.append(sum(x)/len(x))
+
+    arr = sorted(res, reverse=True)
+
+    for x in res:
+        answer.append(arr.index(x)+1)
+
+    return answer
