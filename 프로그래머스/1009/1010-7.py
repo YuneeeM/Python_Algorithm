9a5c040c12e5e959203e23855707d52666348cc9
YuneeeM <dana512@naver.com>
2023-10-12 15:15:07 +0900

    1010 마무리

--- 프로그래머스/1009/1010-7.py
+++ 프로그래머스/1009/1010-7.py
@@ -1,25 +1,9 @@
 # 연속된 수의 합
-num = 5
-total = 5
-arr = []
-
-mid = total//num
-
-i = mid+1
-j = mid-1
-
-while sum(arr) < total:
-
-    if sum(arr)+i <= total:
-        arr.append(i)
-        i += 1
-        print(arr)
-    if sum(arr)+j <= total:
-        arr.append(j)
-        j -= 1
-        print(j)
-    if sum(arr) == total:
-        break
-
-arr.sort()
-print(arr)
+def solution(num, total):
+    answer = []
+    mid = total//num
+    a = mid - ((num - 1)//2)  # 중앙값에서 -1, +1 이런식으로 풀면됨
+    b = mid+((num+2)//2)  # num이 짝수인 경우를 고려하고  range를 고려해서 오른쪽은 +2로 계산
+    for x in range(a, b):
+        answer.append(x)
+    return answer
--- 프로그래머스/1009/1010-8.py
+++ 프로그래머스/1009/1010-8.py
@@ -1,1 +1,21 @@
 # 다음에 올 숫자
+def solution(common):
+    answer = 0
+    arr = []
+
+    for i in range(len(common)-1):
+        arr.append(common[i+1]-common[i])
+
+    arr = set(arr)
+
+    if len(arr) == 1:
+        arr = list(arr)
+        answer = common[0]+(len(common))*arr[0]
+    else:
+        arr = []
+        for i in range(len(common)-1):
+            arr.append(common[i+1]//common[i])
+
+        answer = common[0]*(arr[0]**(len(common)))
+
+    return answer
