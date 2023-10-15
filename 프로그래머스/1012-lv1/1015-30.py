7f340f321e145dc7d469c88e887652ed3f3bdbc3
YuneeeM <dana512@naver.com>
2023-10-15 22:01:18 +0900

    1015 완료

--- 프로그래머스/1012-lv1/1015-30.py
+++ 프로그래머스/1012-lv1/1015-30.py
@@ -1,1 +1,16 @@
 # 최대공약수와 최소공배수
+def solution(n, m):
+    answer = []
+    res = []
+    for i in range(1, min(n, m)+1):
+        if n % i == 0 and m % i == 0:
+            res.append(i)
+
+    for i in range(max(n, m), (n*m)+1):
+        if i % n == 0 and i % m == 0:
+            min_val = i
+            break
+    max_val = max(res)
+    answer.append(max_val)
+    answer.append(min_val)
+    return answer
