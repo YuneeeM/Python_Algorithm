ae92943781f7394ea73cd2117b6f58233554dc20
YuneeeM <dana512@naver.com>
2023-10-26 00:46:41 +0900

    1025

--- 프로그래머스/1016-lv1/1025-11.py
+++ 프로그래머스/1016-lv1/1025-11.py
@@ -1,4 +1,11 @@
 # K번째수
 def solution(array, commands):
     answer = []
+    for x in range(len(commands)):
+        i, j, k = commands[x][0], commands[x][1], commands[x][2]
+        arr = array
+        arr = arr[i-1:j]
+        arr.sort()
+        answer.append(arr[k-1])
+
     return answer
