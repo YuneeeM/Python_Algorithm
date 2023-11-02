018c1a3b72494228b3bc96895c41cb5aa2a454e8
YuneeeM <dana512@naver.com>
2023-11-02 15:21:40 +0900

    1031 - 1

--- 프로그래머스/1031-lv1/1031-1.py
+++ 프로그래머스/1031-lv1/1031-1.py
@@ -1,1 +1,27 @@
-#카드 뭉치
+# 카드 뭉치
+from collections import deque
+
+
+def solution(cards1, cards2, goal):
+    answer = ''
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+    goal = deque(goal)
+
+    while goal:
+        if cards1 and cards1[0] == goal[0]:
+            goal.popleft()
+            cards1.popleft()
+        elif cards2 and cards2[0] == goal[0]:
+            goal.popleft()
+            cards2.popleft()
+        else:
+            answer = 'NO'
+            break
+
+    if not goal:
+        answer = 'Yes'
+    else:
+        answer = 'No'
+
+    return answer
