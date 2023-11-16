e12bc20d03fda5599ebbc329610c59442c5ce0aa
YuneeeM <dana512@naver.com>
2023-11-16 17:31:00 +0900

    1116 - 1

---
+++ 프로그래머스/lv2/1115-lv2/1116-4.py
@@ -0,0 +1,13 @@
+#올바른 괄호
+def solution(s):
+    stack = []
+    
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    
+    return len(stack) == 0
