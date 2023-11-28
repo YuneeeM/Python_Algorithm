a94104f2ac15ffde62e80408de014067d66a8f26
YuneeeM <dana512@naver.com>
2023-11-28 15:58:24 +0900

    1128 - 1

---
+++ Book_Coding/ch7/0703-1.py
@@ -0,0 +1,23 @@
+#빗물 트래핑 - 투 포인터를 최대로 이동
+'''
+높이를 입력받아 비 온 후 얼마나 많은 물이 쌓일 수 있는지 계산하기
+'''
+def trap(self, height:list[int])-> int:
+  if not height:
+    return 0
+  
+  volume = 0
+  left, right = 0, len(height)-1
+  left_max, right_max = height[left], height[right]
+
+  while left < right :
+    left_max,right_max = max(height[left],left_max), max(height[right], right_max)
+    
+    #더 높은 쪽을 향해 투 포인터 이동
+    if left_max <= right_max:
+      volume+=left_max-height[left]
+      left+=1
+    else:
+      volume+=right_max-height[right]
+      right-=1
+  return volume
