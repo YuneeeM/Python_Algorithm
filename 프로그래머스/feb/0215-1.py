16c3ff99de71cdd05e9952f89441664a59deb13d
yuni <dana512@naver.com>
2024-02-15 13:06:33 +0900

    0215 수정

--- 프로그래머스/feb/0215-1.py
+++ 프로그래머스/feb/0215-1.py
@@ -6,8 +6,10 @@ def solution(begin, target, words):
         return 0
     return bfs(begin,target,words)
 
+#최소 단계를 찾아야 하므로 bfs
 def bfs(begin,target,words):
     q = deque()
+    #시작 단어와 단계 0으로 초기화
     q.append((begin,0))
     
     while q:
@@ -16,9 +18,12 @@ def bfs(begin,target,words):
         if now == target:
             return step
         
+        #단어를 모두 체크하면서, 해당 단어가 변경될 수 있는지 체크
         for word in words:
             cnt = 0
+            #단어의 길이만큼 반복하여
             for i in range(len(now)):
+                #단어에 알파벳 한개씩 체크하기
                 if now[i] != word[i]:
                     cnt+=1
             if cnt == 1:
