9266fac51cad952f3b9482bc606de2ba09c354b0
YuneeeM <dana512@naver.com>
2024-03-06 22:16:06 +0900

    0306-1

---
+++ BOJ/march/0306-2798.py
@@ -0,0 +1,16 @@
+# 블랙잭
+n, m = map(int, input().split())
+black = list(map(int, input().split()))
+
+answer = []
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            res = black[i]+black[j]+black[k]
+            if res > m:
+                continue
+            else:
+                answer.append(res)
+
+print(max(answer))
