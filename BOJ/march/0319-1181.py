27d596ed8deae8386305240ea2d50c275ac0c997
YuneeeM <dana512@naver.com>
2024-03-19 23:40:34 +0900

    0319-2

--- BOJ/march/0319-1181.py
+++ BOJ/march/0319-1181.py
@@ -1,1 +1,14 @@
 # 단어 정렬 - 문자열/정렬
+n = int(input())
+arr = []
+for _ in range(n):
+    st = input()
+    arr.append(st)
+
+arr = list(set(arr))
+arr.sort()
+
+arr.sort(key=len)
+
+for k in arr:
+    print(k)
