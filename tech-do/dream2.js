b9da5ecdb9236375895b89e64aa1c4f5b4836c51
YuneeeM <dana512@naver.com>
2024-05-17 00:47:34 +0900

    [0516] tech 준비2

---
+++ tech-do/dream2.js
@@ -0,0 +1,31 @@
+//2. 데이터타입 - 자료형
+
+/*
+ * Boolean
+ * Null
+ * Undefined
+ * Number
+ * String
+ * Symbol(new in ECMAScript 6)
+ * and Object
+ */
+
+console.log(1);
+//산술연산자
+console.log(1 + 1);
+console.log(2 - 1);
+console.log(4 / 2);
+console.log(4 * 2);
+
+//문자열
+console.log("Hello world");
+console.log('Hello world');
+//String.length - properties
+console.log('Hello world'.length);
+//toUpperCase - method (대문자 변환)
+console.log('Hello world'.toUpperCase());
+//indexof (데이터의 위치 변환)
+console.log('Hello world'.indexOf('o'));
+
+//문자열+문자열 = 문자열
+console.log("2" + "1");
