bde3dcd0ee5b82d7213cd27bdf9f09f024b4b17f
YuneeeM <dana512@naver.com>
2024-05-18 00:38:48 +0900

    [0517] tech 준비3

---
+++ tech-do/dream3.js
@@ -0,0 +1,13 @@
+//3. 배열
+
+var coworkers = ["egoing", "leexche"];
+
+console.log(coworkers[0]);
+console.log(coworkers[1]);
+console.log(coworkers.length);
+
+coworkers.push("duru");
+coworkers.push("taeho");
+
+console.log(coworkers.length);
+
