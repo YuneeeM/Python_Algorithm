8e187618daeec60d9d343b0ace97d2d662a1e395
YuneeeM <dana512@naver.com>
2024-05-19 01:55:13 +0900

    [0518] tech 준비4

---
+++ tech-do/dream4.js
@@ -0,0 +1,19 @@
+//4. 객체 - 값을 받는 그릇이라는 관점
+
+var grades = { 'egoing': 10, 'k8805': 6, 'sorialgi': 80 }; //key-value
+
+var grades1 = {};
+grades1['egoing'] = 10;
+grades1['k8805'] = 6;
+grades1['sorialgi'] = 80;
+
+console.log(grades['egoing']);
+console.log(grades['ego' + 'ing']);
+//하지만 grades.'ego'+'ing'; 는 안된다!
+console.log(grades.egoing); //더 간결함
+
+//----------------------------------------------
+
+for (key in grades) {
+  console.log("key : " + key + " value : " + grades[key]);
+}
